712734e74f6df3471e471093b49287342de5fc5e
Vitaly Chasovskikh <>
2024-05-07 02:08:32 +0700

    add test for generate_diff()

---
+++ tests/test_gendiff.py
@@ -0,0 +1,7 @@
+from gendiff.commands.gendiff import generate_diff
+
+
+def test_generate_diff():
+    with open('tests/fixtures/expected1.txt', 'r') as e1:
+        str = e1.read()
+        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str
