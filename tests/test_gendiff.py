040c15eb6d6fca6672d08b1985bbb54ad86ee592
Vitaly Chasovskikh <>
2024-05-08 16:14:38 +0700

    style: test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,6 +4,9 @@ from gendiff.commands.gendiff import generate_diff
 def test_generate_diff():
     with open('tests/fixtures/expected1.txt', 'r') as e1:
         str = e1.read()
-        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str
-        assert generate_diff('tests/fixtures/file2.json', 'tests/fixtures/file1.json') != str
-        assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml') == str
+        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures'
+                                                          '/file2.json') == str
+        assert generate_diff('tests/fixtures/file2.json', 'tests/fixtures'
+                                                          '/file1.json') != str
+        assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures'
+                                                         '/file2.yml') == str
