9043c18c560502fa00f1884b950973e28c33e331
Vitaly Chasovskikh <>
2024-05-08 15:13:56 +0700

    add test for .yml

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,3 +5,5 @@ def test_generate_diff():
     with open('tests/fixtures/expected1.txt', 'r') as e1:
         str = e1.read()
         assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str
+        assert generate_diff('tests/fixtures/file2.json', 'tests/fixtures/file1.json') != str
+        assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml') == str
